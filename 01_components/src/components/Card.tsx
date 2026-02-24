500f6e63f7184f85354c0a6510d76f0120eb44ff
Sahil Malakar <sahilmalakar150@gmail.com>
2026-02-24 21:46:56 +0530

    components

---
+++ 01_components/src/components/Card.tsx
@@ -0,0 +1,15 @@
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Sahil Malakar</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+          vitae!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
